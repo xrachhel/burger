e420ecd506f909b40e2f06b559790af7dc8ea4e8
Rachel Yeung <rachel.cmyeung@gmail.com>
2020-01-22 21:19:11 -0800

    modified connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-if(process.env.JAWSDB_URL){
+
+if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
-else{
+else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-  })
-}
+      host: "localhost",
+      user: "root",
+      password: "password",
+      database: "burgers_db"
+  });
+};
 
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
 
